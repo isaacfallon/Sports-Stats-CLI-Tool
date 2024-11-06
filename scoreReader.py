3af3492bdc1a0f2887a28469fc576313c05b9f84
isaacfallon <isaac.fallon@gmail.com>
2024-11-06 17:06:17 +1030

    Added proper sorting of the results by division and points while also limiting the resutls by 3

--- scoreReader.py
+++ scoreReader.py
@@ -1,18 +1,21 @@
 # Import pathname module to allow pathname checking/manipulation
 import os.path  
 
-# Function to import csv module and reveal the contents of the csv file name the user has input
+# Function to import csv manipulation module and display the contents of the csv file decided by the user.
 def importFoundCSV():
     print('records:\n')
+    # Import the csv module, open the csv in 'read-only' and set a csv.DictReader object containing the file's rows to a variable.
     import csv
     with open(fileInput, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
-        # For each row in the csv file, we output the desired csv data
-        for row in reader:
+        # The csv.DictReader object is sorted based on row in ascending order and points in descending order. The sorted list is limited to the top three results and saved to a variable.
+        sortedData = sorted(reader, key=lambda row: (row['division'], -int(row['points'])))[:3]
+            #  The variable containing the sorted data is displayed based on the data in its rows.
+        for row in sortedData:
             print("name: " + row['firstname'], row['lastname'])
-            print("details: in division " + row['division'] + " from " + row['date'] + " performing " + row['summary'] + "\n")
+            print("details: in division " + row['division'] + " from " + row['date'] + " performing " + row['summary'] + row['points']+ "\n")
 
-# The program starts by prompting the user for a file name. If the file name exists, run our importFoundCSV() function above to read the contents of the file. 
+# The program is initialised by prompting the user for a file name. If the file name exists, run our importFoundCSV() function above to read the contents of the file. 
 fileInput = input('Please enter the csv file name in this directory: ')
 check_file = os.path.isfile(fileInput)
 if check_file is True:
